5ffdb897a3c648efa24c7e70c970778f9cf7afc1
Duc <duc.ddo22@gmail.com>
2023-07-02 13:21:16 +0200

    setup/teardown function

--- test/test_GridScreen.cpp
+++ test/test_GridScreen.cpp
@@ -2,32 +2,40 @@
 #include <unity.h>
 #include <../src/TouchMenuLib.h>
 
-
+GridScreen* grid;
+RoundButton* roundButton1;
+RoundButton* roundButton2;
+DisplayTFTeSPI* display;
 TouchMenuLib TMLTest(new DisplayTFTeSPI());
 
 void callbackTest (bool) {
   TMLTest.back();
 }
-
-void test_if_elements_dont_overlap(){   //macht noch keinen sinn
-    GridScreen* grid = new GridScreen (3, 3 , COLOR_LIGHT_CYAN);
-
-    RoundButton* roundButton1 = new RoundButton ("", COLOR_RED,  "", COLOR_GREEN, COLOR_YELLOW, callbackTest);
-    RoundButton* roundButton2 = new RoundButton ("", COLOR_RED,  "", COLOR_GREEN, COLOR_YELLOW, callbackTest);
-    auto* display = new DisplayTFTeSPI();
+void setUp() {
+    grid = new GridScreen (3, 3 , COLOR_LIGHT_CYAN);
+    roundButton1 = new RoundButton ("", COLOR_RED,  "", COLOR_GREEN, COLOR_YELLOW, callbackTest);
+    roundButton2 = new RoundButton ("", COLOR_RED,  "", COLOR_GREEN, COLOR_YELLOW, callbackTest);
+    display = new DisplayTFTeSPI();
     grid->setDisplay(display);
     grid->setResolution(display->getWigth(), display->getHeight());
-
-    grid->add(roundButton1,0,0,2,2);
-    TEST_ASSERT_FALSE(grid->add(roundButton2,0,0,2,2));
-
-
-    //TEST_ASSERT(true);
-    
 }
 
-void test_if_gridscreen_size_is_less_equal_the_size_of_the_display_device(){
+void tearDown() { 
+  delete display;
+  delete roundButton2;
+  delete roundButton1;
+  grid = nullptr;
+  delete grid;
+  
+}
 
+void test_if_elements_dont_overlap(){   //macht noch keinen sinn
+    grid->add(roundButton1,0,0,2,2);
+    TEST_ASSERT_FALSE(grid->add(roundButton2,0,0,2,2));
+}
+
+/*void test_if_gridscreen_size_is_less_equal_the_size_of_the_display_device(){
+  
 }
 void test_if_grids_are_at_least_as_big_as_a_minium_size(){
 
@@ -47,7 +55,7 @@ void test_if_the_screen_has_a_display_resolution_before_an_element_can_be_added(
 
 void test_if_three_by_three_screen_places_an_element_indended_to_be_placed_in_the_middle_of_a_screen_in_the_actual_middle_of_the_display(){
 
-}
+}*/
 
 
 void setup()
