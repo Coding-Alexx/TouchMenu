ca22ce58bd0f7f678e2470f6138c934dfdc1fac6
Ibopr0fen <107461849+Ibopr0fen@users.noreply.github.com>
2023-10-05 09:26:23 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ highlight_language = 'c++'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
+# html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
 sys.path.insert(0, os.path.abspath(''))
